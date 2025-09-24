d8fcd1640c2f383403501cb6a7a15c0501fb4d6d
nicolegut <nmg40@duke.edu>
2025-09-24 12:44:04 -0400

    Initial Commit of ARGOS Script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Nicole Gutkowski (nmg40@duke.edu)
+# Date:   Fall 2025
+#--------------------------------------------------------------
+
